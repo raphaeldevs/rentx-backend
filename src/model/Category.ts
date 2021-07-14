2b9edd82104f044b8d1bc3365541300410466725
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-14 01:06:30 -0300

    refactor: add type for category construtor class arguments

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,12 +1,17 @@
 import { v4 as uuid } from "uuid";
 
+interface ICategoryConstrutor {
+  name: string;
+  description: string;
+}
+
 class Category {
   id?: string;
   name: string;
   description: string;
   createdAt?: Date;
 
-  constructor({ name, description }) {
+  constructor({ name, description }: ICategoryConstrutor) {
     if (!this.id) {
       this.id = uuid();
     }
