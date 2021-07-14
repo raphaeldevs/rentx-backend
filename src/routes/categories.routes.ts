61bfac1d1f9546a009579a94a73772f1166bcdfc
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-14 00:17:04 -0300

    feat: list categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,13 @@
 import { Request, Response, Router } from "express";
 
-import Category from "../model/Category";
 import CategoriesRepository from "../repositories/Category";
 
 export const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
-
 categoriesRoutes.get("/", (request: Request, response: Response) => {
+  const categories = categoriesRepository.list();
+
   return response.json(categories);
 });
 
