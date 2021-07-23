59275e38970333065b4a789369d74338ce4c568e
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-23 01:10:14 -0300

    feat: remove tmp files after importing data

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -26,7 +26,10 @@ class ImportCategoryUseCase {
 
           categories.push({ name, description });
         })
-        .on("end", () => resolve(categories))
+        .on("end", () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
         .on("error", (error) => reject(error));
     });
   }
