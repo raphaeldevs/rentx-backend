8bf4b15e77aa708c1cf00081a0b9de93805c4c56
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-07-26 22:13:31 -0300

    feat: create migration to create user table and create user entity

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,35 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column({ name: "driver_license" })
+  driverLicence: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  constructor() {
+    this.id = uuid();
+  }
+}
+
+export default User;
