9fe716777c56108594ac5a3657207753de7a05b1
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-12-07 19:52:38 -0300

    fix: seed function

--- src/shared/typeorm/seed/admin.ts
+++ src/shared/typeorm/seed/admin.ts
@@ -1,20 +1,22 @@
 import { hash } from "bcrypt";
 import { v4 as uuid } from "uuid";
 
-import { createConnection } from "..";
+import { createConnection, getConnection } from "..";
 
 async function create() {
-  const connection = await createConnection("localhost");
+  await createConnection();
+
+  const connection = await getConnection();
 
   const id = uuid();
   const password = await hash("admin", 8);
 
   await connection.query(`
-    INSERT INTO users (id, name, email, password, isAdmin, driver_license, created_at)
+    INSERT INTO users (id, name, email, password, "isAdmin", driver_license, created_at)
     VALUES (
       '${id}',
       'Rapha ADM',
-      'admin@rentx.com.br
+      'admin@rentx.com.br',
       '${password}',
       true,
       '0123456789',
@@ -26,5 +28,5 @@ async function create() {
 (async () => {
   await create();
 
-  console.log("Admin created");
+  console.log("Admin created! 😎");
 })();
